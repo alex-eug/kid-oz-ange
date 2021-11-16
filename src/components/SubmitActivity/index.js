3cdce7d7273298cc919e64fffe48ae0b80701a79
alex-eug <alexandre.eugene31@gmail.com>
2021-11-16 16:27:17 +0100

    yyyy

--- src/components/SubmitActivity/index.js
+++ src/components/SubmitActivity/index.js
@@ -4,9 +4,6 @@ import "./style.css";
 import { Button, Form, Input, Radio, TextArea } from "semantic-ui-react";
 
 export default function SubmitActivity() {
-  useEffect(() => {
-    document.title = "Soumettez votre activité"
- }, []);
   const [title, setTitle] = useState("");
   const [description, setDescription] = useState("");
   const [zipcode, setZipCode] = useState("");
@@ -48,6 +45,7 @@ export default function SubmitActivity() {
     try{
       const responce = await axios.get(`https://geo.api.gouv.fr/communes?nom=${town}&fields=nom,codeDepartement&limit=${limitData}&boost=population`);
       console.log(responce.data)
+      // eslint-disable-next-line array-callback-return
       setDataTown(responce.data)
     }
     catch(error){
@@ -68,6 +66,11 @@ export default function SubmitActivity() {
         const jsx = <li onClick={getName} key={home.code}>{home.nom} ({home.codeDepartement})</li>
         return jsx 
       })
+
+
+
+
+
 
       return res
     }
